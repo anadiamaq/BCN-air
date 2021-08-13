52a0a19594051e11327df527bba9437a1e08033a
Ana Diaz <anadiamaq@gmail.com>
2021-08-13 13:31:11 +0200

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # BCN-air
+
+The purpose of this project is to complete the mid project of Big Data & Machine Learning Bootcamp from CORE Code School (https://github.com/core-school/).
+For the realization of this project I have created an API that takes data from a database with two collections from MongoDB Atlas. In addition, I have created a dashboard that consumes data from the API and generates dynamic and static visualizations.
+
+The data for the creation of the database in Mongodb has been taken from a Kaggle dataset (https://www.kaggle.com/xvivancos/barcelona-data-sets). Besides, I created another small collection from scratching the website of the Barcelona City Council to have data on deaths in the month of November 2018 by districts (https://ajuntament.barcelona.cat/estadistica/castella/Estadistiques_per_temes/Poblacio_i_demografia/Demografia/Defuncions/a2018/t12.htm).
+
+The code for scratching the data on the Barcelona city council website is in /Code. On the other hand, the code where the air quality dataset data is cleaned is also in /Code, as well as a file where I made several graphs to have a previous idea of ​​what the app will show.
+
+During the cleaning of the air quality data, the null elements of the dataset for numerical variables such as the concentration of a compound have been substituted for the mean. Additionally, the missing qualitative data has been filled in according to the X_Value, that is, the concentration of the compound. Thus, according to the WHO (World Health Organization), it considers that from a certain level of a compound, air quality becomes moderate. This information has been extracted from the following links:
+
+- https://www.who.int/es/news-room/fact-sheets/detail/ambient-(outdoor)-air-quality-and-health
+- https://op.europa.eu/webpub/eca/special-reports/air-quality-23-2018/es/
+
+To view the app it is only necessary to run the following files:
+
+- `python3 server.py`
+- `streamlit run app.py`
+
+In a python environment with the previous installation of the packages found in the requirements.txt file.
--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -8,42 +8,149 @@ import altair as alt
 response = requests.get("http://localhost:8080/")
 print(response.text)
 
-st.title("Air quality in BCN")
+st.markdown("<h1 style='text-align: center' >Air quality in BCN</h1>", unsafe_allow_html=True)
 
 db_air = requests.get(" http://localhost:8080/read_air_qa")
 db_air = pd.DataFrame.from_records(db_air.json())
-#db_air
+if st.checkbox('Show air quality Data'):
+   db_air
 
 db_deaths = requests.get(" http://localhost:8080/read_deaths_2018")
 db_deaths = pd.DataFrame.from_records(db_deaths.json())
-#db_deaths
+if st.checkbox('Show deaths Data'):
+   db_deaths
+
+
+stations_o3 = st.sidebar.multiselect("Levels of O3 by station ", db_air['Station'].unique())
+if stations_o3:
+    data = db_air[db_air.Station.isin(stations_o3)] 
+
+    st.vega_lite_chart(data, {
+        'width': 'container',
+        'height': 400,
+        'mark':'circle',
+        'encoding':{
+            'x':{
+              'field':'Station',
+              'type': 'nominal'
+            },
+            'y':{
+              'field':'O3_Value',
+              'type':'quantitative'
+              },
+            'size':{
+              'field':'O3_Value',
+              'type':'quantitative'
+              },
+            'color':{
+              'field':'Station',
+              'type':'nominal'}
+            }
+          }, use_container_width=True)
+
+
+stations_no2 = st.sidebar.multiselect("Levels of NO2 by station ", db_air['Station'].unique())
+if stations_no2:
+    data = db_air[db_air.Station.isin(stations_no2)] 
+
+    st.vega_lite_chart(data, {
+        'width': 'container',
+        'height': 400,
+        'mark':'circle',
+        'encoding':{
+            'x':{
+              'field':'Station',
+              'type': 'nominal'
+            },
+            'y':{
+              'field':'NO2_Value',
+              'type':'quantitative'
+              },
+            'size':{
+              'field':'NO2_Value',
+              'type':'quantitative'
+              },
+            'color':{
+              'field':'Station',
+              'type':'nominal'}
+            }
+          }, use_container_width=True)
+
+
+stations_pm10 = st.sidebar.multiselect("Levels of PM10 by station ", db_air['Station'].unique())
+if stations_pm10:
+    data = db_air[db_air.Station.isin(stations_pm10)] 
+
+    st.vega_lite_chart(data, {
+        'width': 'container',
+        'height': 400,
+        'mark':'circle',
+        'encoding':{
+            'x':{
+              'field':'Station',
+              'type': 'nominal'
+            },
+            'y':{
+              'field':'PM10_Value',
+              'type':'quantitative'
+              },
+            'size':{
+              'field':'PM10_Value',
+              'type':'quantitative'
+              },
+            'color':{
+              'field':'Station',
+              'type':'nominal'}
+            }
+          }, use_container_width=True) 
 
 
 
-chart1 = alt.Chart(db_air).mark_line().encode(
-  x=alt.X('Date'),
-  y=alt.Y('O3_Value'),
-).properties(title="O3 levels over a month")
-st.altair_chart(chart1, use_container_width=True)
+st.caption('This graph shows how O3 concentration in the air at ground level changes over a month (November, 2018). It can cause breathing problems, cause asthma, reduce lung function, and lead to lung disease.')
+st.vega_lite_chart(db_air, {
+  'data': db_air,
+  'width': 700,
+  'height': 400,
+  'mark': 'line',
+  'encoding': {
+    'x': {'timeUnit': 'day', 'field': 'Date'},
+    'y': {'aggregate': 'mean', 'field': 'O3_Value'}
+  }
+})
 
-chart2 = alt.Chart(db_air).mark_line().encode(
-  x=alt.X('Date'),
-  y=alt.Y('NO2_Value'),
-).properties(title="NO2 levels over a month")
-st.altair_chart(chart2, use_container_width=True)
+st.caption('This graph shows how NO2 concentration in the air changes over a month (November, 2018). Epidemiological studies have shown that the symptoms of bronchitis in asthmatic children have increased in European countries due to prolonged exposure to NO2.')
+st.vega_lite_chart(db_air, {
+  'data': db_air,
+  'width': 700,
+  'height': 400,
+  'mark': 'line',
+  'encoding': {
+    'x': {'timeUnit': 'day', 'field': 'Date'},
+    'y': {'aggregate': 'mean', 'field': 'NO2_Value'}
+  }
+})
 
-chart2 = alt.Chart(db_air).mark_line().encode(
-  x=alt.X('Date'),
-  y=alt.Y('PM10_Value'),
-).properties(title="PM10 levels over a month")
-st.altair_chart(chart2, use_container_width=True)
 
+st.caption('This graph shows how PM10 concentration in the air changes over a month (November, 2018). It is estimated that exposure to anthropogenic particles reduces the average life expectancy by 8.6 months.')
+st.vega_lite_chart(db_air, {
+  'data': db_air,
+  'width': 700,
+  'height': 400,
+  'mark': 'line',
+  'encoding': {
+    'x': {'timeUnit': 'day', 'field': 'Date'},
+    'y': {'aggregate': 'mean', 'field': 'PM10_Value'}
+  }
+})
+
+st.caption('Number of deaths during November of 2018')
 chart4 = alt.Chart(db_deaths).mark_bar().encode(
   x=alt.X('Total_deaths'),
   y=alt.Y('Districts'),
 ).properties(title="Total deaths by districts")
 st.altair_chart(chart4, use_container_width=True)
 
+st.caption('Air quality during November of 2018 by distict')
 chart5 = alt.Chart(db_air).mark_bar(
 ).encode(
   x='Station',
@@ -51,3 +158,12 @@ chart5 = alt.Chart(db_air).mark_bar(
   color='Air_Quality:N'
 ).properties(title="Air quality by district")
 st.altair_chart(chart5, use_container_width=True)
+
+
+import numpy as np
+
+df = pd.DataFrame(
+  np.random.randn(5744, 2) / [50, 50] + [41.3879, 2.16992],
+  db_air, columns=["latitude", "longitude"])
+
+st.map(df)
